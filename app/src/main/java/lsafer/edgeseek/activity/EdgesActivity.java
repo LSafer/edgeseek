49153a923c302d0271f9795f37e71cec55215684
LSafer <lsaferse@gmail.com>
2020-06-08 14:26:14 +0300

    Update EdgesActivity.java

--- app/src/main/java/lsafer/edgeseek/activity/EdgesActivity.java
+++ app/src/main/java/lsafer/edgeseek/activity/EdgesActivity.java
@@ -28,7 +28,7 @@ import java.util.Objects;
 import lsafer.edgeseek.App;
 import lsafer.edgeseek.R;
 import lsafer.edgeseek.data.EdgeData;
-import lsafer.edgeseek.util.Util;
+import lsafer.edgeseek.util.Position;
 
 /**
  * An activity that makes the user navigate between the preference foreach edge.
@@ -42,24 +42,40 @@ final public class EdgesActivity extends AppCompatActivity {
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		//initial
 		super.onCreate(savedInstanceState);
-		this.setTheme(Util.theme(App.data.theme));
+		this.setTheme(App.data.getTheme());
 		this.setContentView(R.layout.activity_edges);
+
+		this.findViewById(R.id.bottom_split).setOnLongClickListener(this::onSplitLongClick);
+		this.findViewById(R.id.left_split).setOnLongClickListener(this::onSplitLongClick);
+		this.findViewById(R.id.top_split).setOnLongClickListener(this::onSplitLongClick);
+		this.findViewById(R.id.right_split).setOnLongClickListener(this::onSplitLongClick);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
+		//SET `GONE` TO ALL FACTORS
+		for (int side : Position.SIDES)
+			for (int factor : Position.FACTORS)
+				this.findViewById(Position.getId(side, factor))
+						.setVisibility(View.GONE);
+
+		//SET `VISIBLE` TO ACTIVE FACTORS
+		for (int i = 0; i < Position.SIDES.length; i++)
+			this.findViewById(Position.getId(i, App.data.sides.get(i).factor))
+					.setVisibility(View.VISIBLE);
+
 		//styling
-		for (EdgeData data : App.data.edges)
-			this.findViewById(Util.id(data.position))
-					.setBackgroundColor(!data.activated ?
-										Color.TRANSPARENT :
-										Color.argb(150,
-												Color.red(data.color),
-												Color.green(data.color),
-												Color.blue(data.color))
-					);
+		for (EdgeData data : App.data.edges) {
+			View view = this.findViewById(Position.getId(data.position));
+
+			if (data.activated)
+				view.setBackgroundColor(Color.argb(150,
+						Color.red(data.color),
+						Color.green(data.color),
+						Color.blue(data.color)));
+		}
 	}
 
 	/**
@@ -69,10 +85,26 @@ final public class EdgesActivity extends AppCompatActivity {
 	 * @throws NullPointerException if the given 'view' is null
 	 */
 	public void onEdgeClick(View view) {
-		//click listener
 		Objects.requireNonNull(view, "view");
+
 		Intent intent = new Intent(this, EdgeActivity.class);
-		intent.putExtra("edge", Util.position(view.getId()));
+		intent.putExtra("edge", Position.fromId(view.getId()));
 		this.startActivity(intent);
+	}
+
+	/**
+	 * Get invoked when an edge-splitter get long-clicked.
+	 *
+	 * @param view the view that have been clicked
+	 * @return true if the callback consumed the long click, false otherwise.
+	 * @throws NullPointerException if the given 'view' is null
+	 */
+	public boolean onSplitLongClick(View view) {
+		Objects.requireNonNull(view, "view");
+
+		Intent intent = new Intent(this, SideActivity.class);
+		intent.putExtra("side", Position.fromId(view.getId()));
+		this.startActivity(intent);
+		return true;
 	}
 }
