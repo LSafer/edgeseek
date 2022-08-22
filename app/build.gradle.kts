3bf28eb534bb502df8d65e96497e044d8db3dae7
Sulaiman Oboody <lsafer@cufy.org>
2022-08-23 00:01:57 +0300

    build: Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lsafer.edgeseek"
         minSdk = 24
         targetSdk = 32
-        versionCode = 11
-        versionName = "0.2.1"
+        versionCode = 12
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
