a58dd20a1e211d9fc85f606e34a9dc40f3a15fb1
Sulaiman Oboody <lsafer@cufy.org>
2025-04-04 21:29:18 +0300

    build: Updated version to 16 (0.3-pre.2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 }
 
 group = "net.lsafer.edgeseek"
-version = "0.3-pre.1"
-project.extraProperties.set("version_code", 15)
+version = "0.3-pre.2"
+project.extraProperties.set("version_code", 16)
 project.extraProperties.set("application_id", "lsafer.edgeseek")
 
 tasks.wrapper {
