00dcf52455da044d3e24261a0bee8919e74e0eee
Sulaiman Oboody <lsafer@cufy.org>
2025-05-02 15:19:49 +0300

    fix: Android API 24 crash on open

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,10 +136,16 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
